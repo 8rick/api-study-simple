be8ed2cc69e2109d3544d0ec5296a2ebd0aef6ea
Rickelmy Barbosa <142022506+8rick@users.noreply.github.com>
2026-08-24 19:30:14 -0300

    fix: correct MockMvc test imports

--- src/test/java/estudo/spring_java/controller/ProdutoControllerTest.java
+++ src/test/java/estudo/spring_java/controller/ProdutoControllerTest.java
@@ -6,7 +6,6 @@ import estudo.spring_java.service.ProdutoService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.boot.webmvc.test.mockmvc.AutoConfigureMockMvc;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,7 +18,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ProdutoController.class)
-@AutoConfigureMockMvc
 class ProdutoControllerTest {
 
     @Autowired
