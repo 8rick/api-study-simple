18224ed6c354d7007d4f00f64442683abf45dc94
Rickelmy Barbosa <142022506+8rick@users.noreply.github.com>
2026-08-24 19:43:40 -0300

    refactor: improve controller readability

--- src/main/java/estudo/spring_java/controller/ProdutoController.java
+++ src/main/java/estudo/spring_java/controller/ProdutoController.java
@@ -6,40 +6,45 @@ import estudo.spring_java.service.ProdutoService;
 import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.*;
-
 import org.springframework.data.domain.Pageable;
-import java.util.List;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/produtos") // Todas as Urls dessa classe começam com /produtos
+@RequestMapping("/produtos")
 public class ProdutoController {
 
     private final ProdutoService service;
 
     public ProdutoController(ProdutoService service) {
-        this.service = service  ;
+        this.service = service;
     }
 
     @GetMapping
-    public Page listarTodos(
+    public Page<?> listarTodos(
             @ParameterObject
             @PageableDefault(size = 5, sort = "nome") Pageable paginacao) {
-
         return service.listarProdutos(paginacao);
     }
 
     @PostMapping
     public Produto criarProduto(@Valid @RequestBody ProdutoDTO dto) {
-         Produto novoProduto = new Produto(dto.getNome(), dto.getPreco());
-         return service.salvar(novoProduto);
+        Produto novoProduto = new Produto(dto.getNome(), dto.getPreco());
+        return service.salvar(novoProduto);
     }
 
     @PutMapping("/{id}")
-    public Produto atualizarProduto(@PathVariable Long id, @RequestBody Produto produto) {
+    public Produto atualizarProduto(
+            @PathVariable Long id,
+            @RequestBody Produto produto) {
         return service.atualizar(id, produto);
-
     }
 
     @DeleteMapping("/{id}")
